f7554f2b88b2437593e0c73d4d89f4a204e8c4ce
amine <akrout.med.amine@gmail.com>
2020-07-06 13:50:06 +0200

    first commit

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -7,17 +7,6 @@ from kafka import KafkaProducer
 API_KEY = "65beeafb8527ee3d2c9bca1ac7ee195f194f7001" # FIXME Set your own API key here
 url = "https://api.jcdecaux.com/vls/v1/stations?apiKey={}".format(API_KEY)
 
-'''
-def on_send_success(record_metadata):
-    print('Topic: ' + record_metadata.topic)
-    print('Partition: ' + record_metadata.partition)
-    print('Offset n°' + str(record_metadata.offset))
-
-def on_send_error(excp):
-    log.error('Error: ', exc_info=excp)
-
-'''
-#producer = KafkaProducer(bootstrap_servers=["172.25.0.12:9093","172.25.0.13:9094"])
 producer = KafkaProducer(bootstrap_servers=["localhost:9092"])
 
 while True:
